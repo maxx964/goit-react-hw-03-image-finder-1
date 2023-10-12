fc44e0a6e67db3dd8a23e645b067b766ec328651
maxx964 <127957496+maxx964@users.noreply.github.com>
2023-10-12 18:19:29 +0300

    ...

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styles from './Modal.module.css';
+
+class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyPress);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyPress);
+  }
+
+  handleKeyPress = (e) => {
+    if (e.code === 'Escape') {
+      this.props.onClose();
+    }
+  };
+
+  handleOverlayClick = (e) => {
+    if (e.target === e.currentTarget) {
+      this.props.onClose();
+    }
+  };
+
+  render() {
+    const { largeImageURL, alt,} = this.props;
+
+    return (
+      <div className={styles.overlay} onClick={this.handleOverlayClick}>
+        <div className={styles.modal}>
+          <img src={largeImageURL} alt={alt} />
+        </div>
+      </div>
+    );
+  }
+}
+
+Modal.propTypes = {
+  largeImageURL: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
+
+export default Modal;
