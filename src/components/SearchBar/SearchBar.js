232c16ee753bcbc937225102957bd2c474abce90
maxx964 <127957496+maxx964@users.noreply.github.com>
2023-10-21 11:32:12 +0300

    ...

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import styles from './SearchBar.module.css';
 import { MdImageSearch } from 'react-icons/md';
-import { handleSubmit } from '../formUtils/formUtils'
+
 class SearchBar extends Component {
   state = {
     query: '',
@@ -12,18 +12,18 @@ class SearchBar extends Component {
     this.setState({ query: e.target.value });
   };
 
- handleSubmit = (e) => {
-  e.preventDefault();
-  const { query } = this.state;
-  if (query.trim() !== '') {
-    this.props.onSubmit(query);
-  }
-};
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { query } = this.state;
+    if (query.trim() !== '') {
+      this.props.onSubmit(query);
+    }
+  };
 
   render() {
     return (
       <header className={styles.searchbar}>
-   <form className={styles.form} onSubmit={(e) => handleSubmit(e, this.state.query, this.props.onSubmit)}>
+        <form className={styles.form} onSubmit={this.handleSubmit}>
           <button type="submit" className={styles.button}>
             <MdImageSearch size={30} />
           </button>
